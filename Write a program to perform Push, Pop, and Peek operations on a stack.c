e55943586cfc32c5de59f39422e094f97d5d2063
Vivek Sharma <69588275+095viveksharma@users.noreply.github.com>
2022-01-31 13:28:23 +0530

    Create Write a program to perform Push, Pop, and Peek operations on a stack.c

---
+++ Write a program to perform Push, Pop, and Peek operations on a stack.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#define MAX 3
+
+int st[MAX], top= -1;
+
+void push(int st[], int val)
+{
+    if(top == MAX-1)
+    {
+        printf("\n Stack overflow");
+    }
+    else{
+
+        top++;
+        st[top] = val;
+    }
+}
+
+int pop(int st[])
+{
+    int val;
+    if(top == -1)
+    {
+        printf("\n Stack underflow");
+        return -1;
+    }
+    else
+    {
+        val= st[top];
+        top--;
+        return val;
+    }
+}
+int peek(int st[])
+{
+    if(top== -1)
+    {
+        printf("\n Stack is empty");
+        return -1;
+    }
+    else
+        return(st[top]);
+}
+void display(int st[])
+{
+    int i;
+    if(top== -1)
+        printf("\n Stack is Empty");
+    else
+    {
+        for(i=top;i>=0;i--){
+            printf("\n%d",st[i]);
+            printf("\n");
+        }
+    }
+}
+int main(int argc, char *argv[])
+{
+    int val, option;
+    do
+    {
+        printf("\n *****MAIN*****");
+        printf("\n 1. PUSH");
+        printf("\n 2. POP");
+        printf("\n 3. PEEK");
+        printf("\n 4. Display");
+        printf("\n 5. EXIT");
+        printf("\n Enter your option: ");
+        scanf("%d",&option);
+        switch(option)
+        {
+        case 1:
+            printf("\n Enter the number to be pushed on stack: ");
+            scanf("%d", &val);
+            push(st,val);
+            break;
+
+        case 2:
+            val = pop(st);
+            if(val!= -1)
+            printf("\n The deleted from stack is: %d", val);
+            break;
+        case 3:
+            val = peek(st);
+            if(val!= -1)
+            printf("\n The value stored at top of stack is: %d",val);
+            break;
+        case 4:
+            display(st);
+            break;
+        }
+    }while(option !=5);
+
+    return 0;
+}
